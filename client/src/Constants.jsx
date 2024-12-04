d525e830b1a9b070f81309fa32a68132fc4af697
Vinayan AC <vinayanac7777@gmail.com>
2024-12-04 08:50:10 +0530

    api updated

--- client/src/Constants.jsx
+++ client/src/Constants.jsx
@@ -1,15 +1,15 @@
 import axios from "axios";
 
-export const BaseUrl = "http://localhost:1009";
-export const ReferralBaseUrl = "https://mern-ecommerce-i13m.onrender.com/signup";
-const AxiosInstance = axios.create({
-  baseURL: "http://localhost:1009",
-});
-// export const BaseUrl = "https://fivegworld.onrender.com";
-// export const ReferralBaseUrl = "https://fivegworldd.onrender.com/signup";
+// export const BaseUrl = "http://localhost:1009";
+// export const ReferralBaseUrl = "https://mern-ecommerce-i13m.onrender.com/signup";
 // const AxiosInstance = axios.create({
-//   baseURL: "https://fivegworld.onrender.com",
+//   baseURL: "http://localhost:1009",
 // });
+export const BaseUrl = "https://fivegworld.onrender.com";
+export const ReferralBaseUrl = "https://fivegworldd.onrender.com/signup";
+const AxiosInstance = axios.create({
+  baseURL: "https://fivegworld.onrender.com",
+});
 
 AxiosInstance.interceptors.request.use(function (config) {
   const token = localStorage.getItem("admin-token");
